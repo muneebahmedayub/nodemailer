35fe9a970d735db5cc03d59681352ea50f38ecec
muneebahmedayub <muneebahmedayub01@gmail.com>
2023-01-27 18:49:22 +0500

    fixed cors error

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("working!!!");
